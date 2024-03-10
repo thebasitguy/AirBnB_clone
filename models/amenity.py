80f2d5cdc9f9fb29aebfa7fad9746ea74b810569
thebasitguy <mailbasitgh@gmail.com>
2024-03-10 01:10:29 -0800

    new files

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines the Amenity class"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represents an amenity
+
+    Attributes:
+        name (str): Name of the amenity
+    """
+
+    name = ""
+
---
+++ models/review.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines the Review class"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represents a review
+
+    Attributes:
+        place_id (str): Place id
+        user_id (str): User id
+        text (str): Text of the review
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
