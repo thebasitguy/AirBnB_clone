57ae71b6c65b409607120625ab737be5bef8b214
thebasitguy <mailbasitgh@gmail.com>
2024-03-10 01:23:06 -0800

    pycodestyle checks file

---
+++ pycodestyle_check.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Prints out phone numbers in words"""
+
+phone = input("Phone: ")
+digits_mapping = {
+    "0": "Zero"
+    "1": "One"
+    "2": "Two"
+    "3": "Three"
+    "4": "Four"
+    "5": "Five"
+    "6": "Six"
+    "7": "Seven"
+    "8": "Eight"
+    "9": "Nine"
+}
+output = ""
+for ch in phone:
+    output += digits_mapping.get(ch, "!") + " "
+print(output)
